534a893abefc78e97d670988dfb1f748e022b6ef
Logan Heffernan <loganheffernan@logans-mbp.lan>
2020-03-27 18:09:50 -0500

    Update: Nav. changed.

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,36 +1,77 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import { Link } from 'gatsby'
 
-const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
-    {gridItems.map(item => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
+import logo from '../img/logo.svg'
+import facebook from '../img/social/facebook.svg'
+import instagram from '../img/social/instagram.svg'
+import twitter from '../img/social/twitter.svg'
+import vimeo from '../img/social/vimeo.svg'
+
+const Footer = class extends React.Component {
+  render() {
+    return (
+      <footer className="footer has-background-black has-text-white-ter">
+        <div className="content has-text-centered has-background-black has-text-white-ter">
+          <div className="container has-background-black has-text-white-ter">
+            <div className="columns">
+              <div className="column is-4">
+                <section className="menu">
+                  <ul className="menu-list">
+                    <li>
+                      <Link to="/" className="navbar-item">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/about">
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/products">
+                        Products
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact/examples">
+                        Form Examples
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        className="navbar-item"
+                        href="/admin/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Admin
+                      </a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div className="column is-4">
+                <section>
+                  <ul className="menu-list">
+                    <li>
+                      <Link className="navbar-item" to="/blog">
+                        Latest Stories
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact">
+                        Contact
+                      </Link>
+                    </li>
+                  </ul>
+                </section>
+              </div>
             </div>
           </div>
-          <p>{item.text}</p>
-        </section>
-      </div>
-    ))}
-  </div>
-)
-
-FeatureGrid.propTypes = {
-  gridItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-      text: PropTypes.string,
-    })
-  ),
+        </div>
+      </footer>
+    )
+  }
 }
 
-export default FeatureGrid
+export default Footer
